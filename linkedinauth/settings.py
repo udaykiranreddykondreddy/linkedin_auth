5d7796e196bc26f9268398a9dc13a7eca13f8b34
asshole <asshole@localhost.localdomain>
2017-12-08 19:07:03 +0530

    As

--- linkedinauth/settings.py
+++ linkedinauth/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +25,7 @@ SECRET_KEY = '3^3cml*85wa@j2qznf_y#+b(l5iuk%s$t454_*-!)!-52e^joh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,12 +76,10 @@ WSGI_APPLICATION = 'linkedinauth.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+              'default': dj_database_url.config(
+                            default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
+                                  )
+                }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
